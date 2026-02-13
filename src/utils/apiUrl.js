e85f57d04cf7f87c21ebda178e2f35314acc7766
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2026-02-13 22:16:55 +0100

    create url constant for url api

---
+++ src/utils/apiUrl.js
@@ -0,0 +1,21 @@
+
+
+
+//api url pour message
+
+export const messageApi = {
+    api: ""
+};
+
+//project api url
+
+export const projectApi = {
+    api: ""
+};
+
+//skill api url
+
+export const skillsApi = {
+    api: ""
+};
+
