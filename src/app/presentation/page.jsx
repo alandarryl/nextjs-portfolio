0f97584fad6ee7830cb812bf0d1b6ef5debf6acc
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2026-02-15 17:58:12 +0100

    latest version update of the portfolio with the text in presentation edited

--- src/app/presentation/page.jsx
+++ src/app/presentation/page.jsx
@@ -25,18 +25,43 @@ export default function AboutPage() {
               Je suis en deuxième année de Bachelor en développement web. Je fais
               preuve d’une grande capacité d’adaptation et d’un bon esprit d’équipe.
               J’aime apprendre et résoudre des problèmes.
+              <br/><br/>
+              Intégration web (HTML, CSS, JavaScript,
+              Tailwind CSS, responsive design) ;<br/><br/>
+              Développement front-end (React et autre
+              framework js) ;<br/><br/>
+              Développement back-end (PHP, node js,
+              Express js, MongoDB, MySQL et autres
+              selon les projets) ;<br/><br/>
+              Utilisation de CMS et outils de no-code
+              (WordPress, etc) ;<br/><br/>
+              Git, Trello, Notion de déploiement
+              (Netlify, Vercel, GitHub Pages) ;<br/><br/>
+              Maîtrise des outils de développement
+              (Visual Studio Code, Postman, jMerise,
+              Looping, Figma, Linux, Firebase,
+              XAMPP serveur).
             </p>
 
             <h3>mes competences</h3>
             <div className="skills">
+              <div className="skill-item">React js</div>
+              <div className="skill-item">TypeScript</div>
+              <div className="skill-item">Next js</div>
               <div className="skill-item">HTML</div>
+              <div className="skill-item">Express js</div>
+              <div className="skill-item">PHP</div>
+              <div className="skill-item">Python</div>
+              <div className="skill-item">C#</div>
+              <div className="skill-item">Rest API</div>
+              <div className="skill-item">MyQL</div>
+              <div className="skill-item">Mongodb</div>
               <div className="skill-item">CSS</div>
-              <div className="skill-item">SASS</div>
               <div className="skill-item">Bootstrap</div>
               <div className="skill-item">Tailwind</div>
               <div className="skill-item">JavaScript</div>
               <div className="skill-item">Git</div>
-              <div className="skill-item">Adobe XD</div>
+              <div className="skill-item">Docker</div>
             </div>
 
             {/* TIMELINE TABS */}
